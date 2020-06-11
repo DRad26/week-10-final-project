c9c2a15e70cbcce1024ae5dc0720fa0f872baddd
DRad26 <63131803+DRad26@users.noreply.github.com>
2020-06-10 19:50:04 -0700

    Updated links and labeling

--- whiskey.js
+++ whiskey.js
@@ -3,36 +3,30 @@ const Whiskies = [
         "name": "Cadee Distillery",
         "continent": "North America",
         "color": "Brown",
-        "year founded": 2014,
            },
     {
         "name": "Suntory Yamazaki Distillery",
         "continent": "Asia",
         "color": "Tan",
-        "year founded": 2014,
     },
     {
         "name": "Sprinkbank Distillery",
         "continent": "Europe",
         "color": "Yellow",
-        "year founded": 2014,
     },
     {
         "name": "James Sedgwick Distillery",
         "continent": "Africa",
         "color": "Yellow",
-        "year founded": 2014,
     },
     {
         "name": "Sullivan's Cove",
         "continent": "Australia",
         "color": "Brown",
-        "year founded": 2014,
     },
     {
         "name": "Natu Nobilis Distillery",
         "continent": "Action,Adventure,Sci-Fi",
         "color": "Tan",
-        "year founded": 2014,
     },
 ]
